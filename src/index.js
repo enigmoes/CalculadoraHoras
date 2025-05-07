6475188ed24b93e45614a556ca7fa1f6f825ee0d
Enigmo <enigmo@enigmo.es>
2025-05-07 11:09:13 +0200

    Corregir identación en index.js

--- src/index.js
+++ src/index.js
@@ -28,28 +28,28 @@ if (!app.isPackaged) {
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-  app.quit();
+    app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 500,
-    height: 350,
-    resizable: false,
-	  maximizable: false,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    }
-  });
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 500,
+        height: 350,
+        resizable: false,
+        maximizable: false,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        }
+    });
 
-  // Load icon
+    // Load icon
 	mainWindow.setIcon(path.join(__dirname, '../assets/img/icon.png'));
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+    // and load the index.html of the app.
+    mainWindow.loadFile(path.join(__dirname, 'app/index.html'));
 
-  // Menu
+    // Menu
 	const mainMenu = Menu.buildFromTemplate(templateMenu);
 	// Set The Menu to the Main Window
 	Menu.setApplicationMenu(mainMenu);
@@ -64,37 +64,34 @@ const createWindow = () => {
 			console.log('Copiado!')
 		}
 	}));
-  // Set ContextMenu
+    // Set ContextMenu
 	mainWindow.webContents.on('context-menu',(e, params) => {
 	  ctxMenu.popup(mainWindow, params.x, params.y)
 	});
-
-  // Set native theme light
-	// nativeTheme.themeSource = 'dark';
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow();
+    createWindow();
 
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 // In this file you can include the rest of your app's specific main process
